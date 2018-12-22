bb289a5e9785895f4009a9cb345bf693953d8eca
gokhn <gokhngungor@gmail.com>
2018-12-22 16:05:18 +0300

    Proje Eklendi

---
+++ MacYorumAndroid/app/src/main/java/yorum/mac/com/macyorumandroid/SignUpActivity.java
@@ -0,0 +1,44 @@
+package yorum.mac.com.macyorumandroid;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class SignUpActivity extends Activity {
+
+    EditText mail,mophone,pswd,usrusr;
+    TextView lin,sup;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup);
+
+        sup = (TextView) findViewById(R.id.sup);
+        lin = (TextView) findViewById(R.id.lin);
+        usrusr = (EditText) findViewById(R.id.usrusr);
+        pswd = (EditText) findViewById(R.id.pswrdd);
+        mail = (EditText) findViewById(R.id.mail);
+        mophone = (EditText) findViewById(R.id.mobphone);
+        Typeface custom_font = Typeface.createFromAsset(getAssets(), "deneme/LatoLight.ttf");
+        Typeface custom_font1 = Typeface.createFromAsset(getAssets(), "deneme/LatoRegular.ttf");
+        mophone.setTypeface(custom_font);
+        sup.setTypeface(custom_font1);
+        pswd.setTypeface(custom_font);
+        lin.setTypeface(custom_font);
+        usrusr.setTypeface(custom_font);
+        mail.setTypeface(custom_font);
+        lin.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent it = new Intent(SignUpActivity.this,LoginActivity.class);
+                startActivity(it);
+            }
+        });
+    }
+}
